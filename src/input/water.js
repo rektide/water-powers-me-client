98d0a3bd9b64ec4e6cc7ed1bd1e12de8125d3759
Matthew Fowle <matthewf@webs.com>
2016-04-27 17:59:11 -0400

    Start flexing.

--- src/input/water.js
+++ src/input/water.js
@@ -2,6 +2,7 @@ let
   React= require("react"),
   card= require("react-mdl/lib/Card"),
   Card= card.Card,
+  CardActions= card.CardActions,
   CardTitle= card.CardTitle,
   CardText= card.CardText,
   Slider= require("react-mdl/lib/Slider").default,
@@ -14,17 +15,24 @@ let WaterInput= React.createClass({
 	},
 	render: function(props){
 		return (<Card shadow={0} style={{width: "540px", margin: "10px 30px"}} >
-				<CardTitle style={{color: "#33a"}}>Record a Drink</CardTitle>
-				<div style={{display:"flex"}}>
-					<Slider style={{"flexGrow": 3}} min={0} max={32} value={this.state.oz} onChange={this.slide} />
-					<CardText>{this.state.oz}</CardText>
-					<Button raised ripple onClick={this.submit}>Drink</Button>
-				</div>
-				<div>
+				<CardTitle style={{color: "#33a"}}>
+					Record a Drink
+				</CardTitle>
+				<CardText style={{display: "flex"}}>
+					<div style={{flex: "0 1 auto", textAlign: "center"}}>
+						<span style={{fontSizeAdjust: 0.7, fontWeight: "bolder"}}>
+							oz</span><br/>
+						{this.state.oz}
+					</div>
+					<Slider style={{flex: "0 1 auto"}} min={0} max={32} value={this.state.oz} onChange={this.slide} />
+					<Button style={{flex: "0 1 auto"}} raised ripple onClick={this.submit}>Drink</Button>
+				</CardText>
+				<CardActions border style={{display: "flex"}}>
 					<Button raised ripple onClick={this.setter(6)}>Cup (6oz)</Button>
 					<Button raised ripple onClick={this.setter(9)}>Glass (9oz)</Button>
 					<Button raised ripple onClick={this.setter(12)}>Tall (12oz)</Button>
-				</div>
+				</CardActions>
+
 			</Card>)
 	},
 	slide: function(e){
